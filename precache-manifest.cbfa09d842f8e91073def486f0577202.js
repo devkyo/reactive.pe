492fa20e82d68b5bbd6d5c036498818c45cfef7e
devkyo <jose@reactive.pe>
2020-05-13 23:01:07 -0500

    Updates

---
+++ precache-manifest.cbfa09d842f8e91073def486f0577202.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40c5caf5c833d9f56a3bee247723ab7",
+    "url": "/reactive.pe/index.html"
+  },
+  {
+    "revision": "e5cd97039c37a9d5966e",
+    "url": "/reactive.pe/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "522be17f5fb26493c712",
+    "url": "/reactive.pe/static/css/main.189f2a81.chunk.css"
+  },
+  {
+    "revision": "e5cd97039c37a9d5966e",
+    "url": "/reactive.pe/static/js/2.b779a201.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactive.pe/static/js/2.b779a201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522be17f5fb26493c712",
+    "url": "/reactive.pe/static/js/main.96b9d9e9.chunk.js"
+  },
+  {
+    "revision": "dc3fa338e73dbb531680",
+    "url": "/reactive.pe/static/js/runtime-main.d0920f42.js"
+  },
+  {
+    "revision": "9b957c72fb52ab8507e99cbea85807e6",
+    "url": "/reactive.pe/static/media/Averta Black.9b957c72.woff"
+  },
+  {
+    "revision": "af89246957cdaa7833d468b17f92b319",
+    "url": "/reactive.pe/static/media/Averta Extra Bold.af892469.woff"
+  },
+  {
+    "revision": "836e325e20aca9975d9cf5c8ee8c54b3",
+    "url": "/reactive.pe/static/media/Averta Light.836e325e.woff"
+  },
+  {
+    "revision": "c638ae0874bcff6a831704bec23ed09f",
+    "url": "/reactive.pe/static/media/Averta Regular Italic.c638ae08.woff"
+  },
+  {
+    "revision": "8b8c9bf9314cd2c8071f607f22060685",
+    "url": "/reactive.pe/static/media/Averta Semibold.8b8c9bf9.woff"
+  },
+  {
+    "revision": "56a5d39fcd8d4f78c10388e7d08c7ba7",
+    "url": "/reactive.pe/static/media/Averta Thin.56a5d39f.woff"
+  },
+  {
+    "revision": "5ee63a5ea3c608bd7ffc448fbb01781d",
+    "url": "/reactive.pe/static/media/Averta.5ee63a5e.woff"
+  },
+  {
+    "revision": "9747c2216b2edf059481d6d212864734",
+    "url": "/reactive.pe/static/media/Gilroy-Bold.9747c221.woff"
+  },
+  {
+    "revision": "0e2ee5593d6806aed16dd6edd3bdd673",
+    "url": "/reactive.pe/static/media/Gilroy-Heavy.0e2ee559.woff"
+  },
+  {
+    "revision": "ef3d6f936e4825929a849b16abeac0ec",
+    "url": "/reactive.pe/static/media/Gilroy-Light.ef3d6f93.woff"
+  },
+  {
+    "revision": "d35dfeb079581284803973c49177dfc6",
+    "url": "/reactive.pe/static/media/Gilroy-Medium.d35dfeb0.woff"
+  },
+  {
+    "revision": "b963b73c1c9b881a10faf8b4d363b85b",
+    "url": "/reactive.pe/static/media/Gilroy-Regular.b963b73c.woff"
+  },
+  {
+    "revision": "af9228de82676ff31d11f645f092d94d",
+    "url": "/reactive.pe/static/media/haywardflight.af9228de.png"
+  },
+  {
+    "revision": "70b4c1ffda716ab1afcb53b65080826e",
+    "url": "/reactive.pe/static/media/hosthinks.70b4c1ff.png"
+  },
+  {
+    "revision": "b8dd97f0f9e86e14f871627bbcba2434",
+    "url": "/reactive.pe/static/media/reactive.b8dd97f0.svg"
+  }
+]);
